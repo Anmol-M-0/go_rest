04dbe9d9f965084121244a86bd3e9d214de1623e
Anmol-M-0 <amaniyar000@gmail.com>
2021-05-13 17:16:28 +0530

    tested all the http verbs

--- src/main.go
+++ src/main.go
@@ -6,13 +6,51 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+func PostHomePage(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "Post Home Page",
+	})
+}
+func HomePage(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "Home page",
+	})
+}
+func PutHomePage(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "Put Home Page",
+	})
+}
+func DeleteHomePage(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "delete Home Page",
+	})
+}
+func PatchHomePage(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "Patch Home Page",
+	})
+}
+func HeadHomePage(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "Head Home Page",
+	})
+	// fmt.Println("in head")
+}
+func OptionsHomePage(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "Options Home Page",
+	})
+}
 func main() {
 	fmt.Println("hello world")
 	r := gin.Default()
-	r.GET("/", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "Hello world",
-		})
-	})
+	r.GET("/", HomePage)
+	r.POST("/", PostHomePage)
+	r.PUT("/", PutHomePage)
+	r.DELETE("/", DeleteHomePage)
+	r.PATCH("/", PatchHomePage)
+	r.HEAD("/", HeadHomePage)
+	r.OPTIONS("/", OptionsHomePage)
 	r.Run()
 }
