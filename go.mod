e8e187f7610e303de821ac2bda09c81196c0beae
Anmol-M-0 <amaniyar000@gmail.com>
2021-05-13 17:00:11 +0530

    created a hello world endpoint

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/Anmol-M-0/go_rest
 
 go 1.16
+
+require github.com/gin-gonic/gin v1.7.1
--- src/main.go
+++ src/main.go
@@ -2,8 +2,17 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	fmt.Println("hello world")
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "Hello world",
+		})
+	})
+	r.Run()
 }
